197225ebd59b32ea2d5f7ad2fa373deb182fa002
mahe1999mh <mahe1999mh@gmail.com>
2024-03-17 17:46:48 +0530

    added card

--- src/Pages/service/service.jsx
+++ src/Pages/service/service.jsx
@@ -11,15 +11,17 @@ function CarServiceList() {
   // Function to render the list of car services
   const renderCarServices = () => {
     return carServices.map((service, index) => (
-      <div key={index} style={{  boxShadow: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px', padding: '20px', marginBottom: '20px' }}>
-        <h3>{service.name}</h3>
-        <p>{service.description}</p>
+      <div style={{display:"flex"}}>      
+        <div key={index} style={{ width:"50vh",height:"30vh",  boxShadow: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px', padding: '20px', marginBottom: '20px' }}>
+        <h1>{service.name}</h1>
+        <p style={{color:"green"}}>{service.description}</p>
+      </div>
       </div>
     ));
   };
 
   return (
-    <div>
+    <div style={{display:"flex",gap:"10px"}}>
       <h2>Car Services</h2>
       {renderCarServices()}
     </div>
