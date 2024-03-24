3d929619051c476e0304b3cc6b25fd61c50ba8f7
mahe1999mh <mahe1999mh@gmail.com>
2024-03-24 22:52:16 +0530

    added payment option

--- src/Pages/service/service.jsx
+++ src/Pages/service/service.jsx
@@ -3,17 +3,22 @@ import React from 'react';
 function CarServiceList() {
   // Define the array of car services
   const carServices = [
-    { name: "Car Washing", description: "Exterior and interior cleaning of the car." },
-    { name: "Wheel Care", description: "Cleaning and maintenance of the wheels and tires." },
-    { name: "Car AC Repair", description: "Repair and maintenance of the car's air conditioning system." }
+    { name: "Car Washing", description: "Exterior and interior cleaning of the car.", img:"https://clipartcraft.com/images/car-wash-clipart-auto-detailing-7.png" },
+    { name: "Wheel Care", description: "Cleaning and maintenance of the wheels and tires.", img:"https://thumbs.dreamstime.com/b/wheel-car-service-logo-141358125.jpg" },
+    { name: "Car AC Repair", description: "Repair and maintenance of the car's air conditioning system.", img:"https://thumbs.dreamstime.com/z/cooling-car-system-thin-line-icon-auto-air-conditioning-vector-illustration-isolated-white-climate-control-outline-style-design-145872885.jpg"}
   ];
 
   // Function to render the list of car services
   const renderCarServices = () => {
     return carServices.map((service, index) => (
       <div style={{display:"flex"}}>      
-        <div key={index} style={{ width:"50vh",height:"30vh",  boxShadow: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px', padding: '20px', marginBottom: '20px' }}>
+        <div key={index} style={{ width:"50vh",height:"40vh",  boxShadow: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px', padding: '20px', marginBottom: '20px' }}>
         <h1>{service.name}</h1>
+        <img style={{width:"250px",height:"150px",objectFit:"fill"}}
+        alt="car"
+        className="image"
+        src={service?.img}
+      />
         <p style={{color:"green"}}>{service.description}</p>
       </div>
       </div>
